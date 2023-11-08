d2caae8b2a09d1e528eed3c3761ab83311767c7a
DavideCiammetti <davideciammetti97@gmail.com>
2023-11-08 15:54:18 +0100

    feat: aggiunta commenti

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
 'use strict';
-
+// aggiungo il contenitore lista UL
 const contenitoreLista = document.getElementById("main-list");
 
 for(let i = 1; i <= 100; i++){
 
     let list = document.createElement('li'); 
-
+    // prendo i numeri che sono sia multipli di 3 che di 5
     if((i % 3 === 0) && (i % 5 === 0)){
     
         console.log('FizzBuzz');
         list.append('FizzBuzz');
         contenitoreLista.append(list);
-
+        // solo multipli di 5
     }else if(i % 5 === 0){
 
         console.log('Buzz');
         list.append('Buzz');
         contenitoreLista.append(list);
-
+        // solo multipli di 3
     }else if(i % 3 === 0){
 
         console.log('Fizz');
         list.append('Fizz');
         contenitoreLista.append(list);
+        // tutto il resto dei numeri
     }else{
 
         console.log(i);
